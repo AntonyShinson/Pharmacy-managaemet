b76113df032f421ac8ad23fcaf76dc7d341c8239
AntonyShinson <160327504+AntonyShinson@users.noreply.github.com>
2025-05-13 18:45:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import sqlite3
 conn = sqlite3.connect("drug_data.db", check_same_thread=False)
 c = conn.cursor()
 
-# Create table if it doesnot exist .
+# CREATE TABLE IF IT DOESNT EXIST
 c.execute('''CREATE TABLE IF NOT EXISTS Customers (
                 C_Name TEXT NOT NULL,
                 C_Password TEXT NOT NULL,
