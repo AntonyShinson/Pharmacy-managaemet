b9a5267eb53e7416d11dd23f52ca3361f89adbc4
AntonyShinson <160327504+AntonyShinson@users.noreply.github.com>
2025-08-06 19:20:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import sqlite3
-# xcaxczxzxczasdasd asdasda adasdas dasdasdas dasasc
+# asdasdcas aacsadc asdcasdcs adcasdasd
 conn = sqlite3.connect("drug_data.db", check_same_thread=False)
 c = conn.cursor()
 
@@ -136,6 +136,7 @@ elif choice == "Admin":
 
 if "logged_in" in st.session_state and st.session_state.logged_in:
     customer_dashboard(st.session_state.username)
+
 
 
 
